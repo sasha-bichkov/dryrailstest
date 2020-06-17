a4ef98a8c002b822fc9e2787f4917c5ecba61a11
Sasha Bychkov <aleksandar.bichkov@gmail.com>
2020-06-17 18:39:54 +0500

    Added result_double

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -6,6 +6,7 @@ describe ApplicationController, type: :controller do
   
   describe '#test' do
     let(:users_new_double) { instance_double(Users::New) }
+    let(:result_double) { double('Result') }
 
     before do
       Dryrailstest::Container.stub('users.new', users_new_double)
@@ -13,7 +14,8 @@ describe ApplicationController, type: :controller do
 
     context 'if age is negative' do
       it 'has status unprocessable entity' do
-        allow(users_new_double).to receive(:call).with(age: 15, name: 'Alexander').and_return(false)
+        allow(users_new_double).to receive(:call).with(age: 15, name: 'Alexander').and_return(result_double)
+        allow(result_double).to receive(:success?).and_return(false)
 
         post :test
 
@@ -23,7 +25,8 @@ describe ApplicationController, type: :controller do
 
     context 'if age is positive' do
       it 'has status ok' do
-        allow(users_new_double).to receive(:call).with(age: 15, name: 'Alexander').and_return(true)
+        allow(users_new_double).to receive(:call).with(age: 15, name: 'Alexander').and_return(result_double)
+        allow(result_double).to receive(:success?).and_return(true)
 
         post :test
 
